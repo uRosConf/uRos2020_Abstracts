85e24594bc74fe66d19cdc45894f8b11c28c2c13
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2020-02-06 13:47:40 +0100

    + wordcloud

--- uRos2020_abstracts/app.R
+++ uRos2020_abstracts/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(data.table)
+library(wordcloud2)
+library(tm)
 
 abst <- readxl::read_excel("abstracts.xlsx")
 setDT(abst)
@@ -7,12 +9,34 @@ titles <- 1:length(abst$Title)
 names(titles) <- as.list(abst$Title)
 types <- c("All", sort(unique(abst[["Type of talk"]])))
 
+wordcloud_data <- function(abstracts) {
+  corp <- Corpus(VectorSource(paste(unlist(abstracts), collapse = "")))
+  corp <- tm_map(corp, removePunctuation)
+  corp <- tm_map(corp, removeNumbers)
+  corp <- tm_map(corp, content_transformer(tolower))
+  corp <- tm_map(corp, removeWords, stopwords("english"))
+  corp <- tm_map(corp, stripWhitespace)
+  m <- as.matrix(TermDocumentMatrix(corp))
+  v <- sort(rowSums(m), decreasing = TRUE)
+  d <- data.frame(
+    word = names(v),
+    freq = v,
+    stringsAsFactors = FALSE)
+
+  d <- d[!d$word %in% c("one", "new", "will", "can", "using", "used", "use"), ]
+  d
+}
+
+suppressWarnings(wcd <- wordcloud_data(abst$Abstract))
+
 ui <- fluidPage(
     titlePanel("uRos 2020 Abstracts"),
     sidebarLayout(
         sidebarPanel(
             selectInput("type", h3("Type of talk"), choices = types),
             selectInput("title", h3("Abstract title"), choices = titles),
+            h3("Contents"),
+            wordcloud2Output("wordcloud")
         ),
         mainPanel(
             fluidRow(
@@ -71,7 +95,7 @@ server <- function(session, input, output) {
     })
     output$curReferences <- renderUI({
         out <- list()
-        
+
         refs <- abst[as.numeric(input$title), References]
         if (!is.na(refs)) {
             out[[1]] <- h3(code("References"))
@@ -79,6 +103,11 @@ server <- function(session, input, output) {
         }
         out
     })
+
+    output$wordcloud <- renderWordcloud2({
+      wordcloud2(data = wcd[wcd$freq > 3, ], "R", size = 0.7)
+    })
+
 
     observeEvent(input$type, {
         if (input$type == "All") {
