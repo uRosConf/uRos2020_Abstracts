3d59649fca06790450211b7225a392ae73520219
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2020-02-06 12:18:38 +0100

    small updates; allow filtering by type of talk

--- uRos2020_abstracts/app.R
+++ uRos2020_abstracts/app.R
@@ -5,59 +5,89 @@ abst <- readxl::read_excel("abstracts.xlsx")
 setDT(abst)
 titles <- 1:length(abst$Title)
 names(titles) <- as.list(abst$Title)
-    
-
-# Define UI for application that draws a histogram
+types <- c("All", sort(unique(abst[["Type of talk"]])))
 
 ui <- fluidPage(
-
-    # Application title
     titlePanel("uRos 2020 Abstracts"),
-
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            selectInput("title",
-                        h3("Abstract title"),
-                        choices = titles)
+            selectInput("type", h3("Type of talk"), choices = types),
+            selectInput("title", h3("Abstract title"), choices = titles),
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           h2(textOutput("curTitle")),
-           h3(textOutput("curAuthor")),
-           htmlOutput("curAbstract")
+            fluidRow(
+                column(
+                    width = 12,
+                    align = "center",
+                    h3(code("Title")), h3(strong(textOutput("curTitle")))
+                )
+            ),
+            fluidRow(
+                column(
+                    width = 12,
+                    align = "center",
+                    h3(code("Authors")), h5(htmlOutput("curAuthor"))
+                )
+            ),
+            fluidRow(
+                column(
+                    width = 12,
+                    align = "center",
+                    h3(code("Content")), h5(htmlOutput("curAbstract"))
+                )
+            ),
+            fluidRow(
+                column(
+                    width = 12,
+                    align = "center",
+                    h3(code("References")), h5(htmlOutput("curReferences"))
+                )
+            )
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-    
+server <- function(session, input, output) {
     output$curTitle <- renderText({
         abst[as.numeric(input$title), Title]
     })
+
     output$curAuthor <- renderText({
-        cur <- abst[as.numeric(input$title), ]    
+        cur <- abst[as.numeric(input$title),]
         curAuthor <- vector()
-        for(i in 1:6){
-            if(!is.na(cur[[paste0("Author",i)]])){
-                curAuthor[i] <- paste0(cur[[paste0("Author",i)]],", ",cur[[paste0("Affiliation",i)]])
+        for (i in 1:6) {
+            auth <- cur[[paste0("Author", i)]]
+            if (!is.na(auth)) {
+                curAuthor[i] <- paste0(auth, " (", cur[[paste0("Affiliation", i)]], ")")
             }
         }
-        paste(curAuthor, collapse=";")
+        paste(curAuthor, collapse = "<br />")
     })
+
     output$curAbstract <- renderUI({
         cur <- abst[as.numeric(input$title), ]
-        out <- list()
-        out[[length(out)+1]] <- p(HTML(gsub("\\n","<br/>",cur[["Abstract"]])))
-        if(!is.na(cur$References)){
-            out[[length(out)+1]] <- h3("References")
-            out[[length(out)+1]] <- p(HTML(gsub("\\n","<br/>",cur[["References"]])))
+        return(p(HTML(gsub("\\n", "<br/>", cur[["Abstract"]]))))
+    })
+    output$curReferences <- renderUI({
+        refs <- abst[as.numeric(input$title), References]
+        if (!is.na(refs)) {
+            out <- p(HTML(gsub("\\n", "<br/>", refs)))
+        } else {
+            out <- "not-available"
         }
         out
     })
+
+    observeEvent(input$type, {
+        if (input$type == "All") {
+            vals <- titles
+        } else {
+            vals <- titles[abst[["Type of talk"]] == input$type]
+        }
+        updateSelectInput(session = session, inputId = "title", choices = vals)
+    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
